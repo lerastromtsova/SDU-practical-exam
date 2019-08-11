bb2c4b01f3cd1626205310684839d84b14275cdf
Валерия Стромцова <v.stromtsova@yandex.ru>
2019-08-11 18:42:58 +0200

    initial tasks

---
+++ 5/setup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+rm -rf exercise
+mkdir exercise
+cd exercise
+git init
+
+
+touch file1
+git add file1
+git commit -am "A"
+
+touch file2
+git add file2
+git commit -am "B"
+
+touch file3
+git add file3
+git commit -am "C"
+
+touch file4
+git add file4
+git commit -am "D"
+
+git checkout HEAD~3
+
+touch file5
+git add file5
+git commit -am "E"
+
+git rebase master
